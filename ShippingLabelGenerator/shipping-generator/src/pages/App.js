230fcc9a3311f9fddd337f3339e9d5baed853ea8
AKARSH SRIVASTAVA <akrsh24@gmail.com>
2018-06-19 17:19:10 +0530

    Updated App.js

--- ShippingLabelGenerator/shipping-generator/src/pages/App.js
+++ ShippingLabelGenerator/shipping-generator/src/pages/App.js
@@ -1,22 +1,26 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router'
-import './App.css'; 
+import './App.css';
 import FirstPage from './FirstPage';
+import Receiver from './Receiver';
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      details:[]
+    this.state = {
+      details: []
     }
   }
+
+
+
   render() {
     return (
       <div>
-         <span><h1>Shipping Label Generator Exercise </h1></span>
-         <FirstPage/>
-         <SecondPage/>
-         <LastPage/>
+        <span><h1>Shipping Label Generator Exercise </h1></span>
+        <Sender name={details.sname} city={details.scity} />
+        <Receiver name={details.rname} city={details.rcity} />
+        <LastPage />
         <button id="start"><Link to='/firstpage'>Start</Link></button>
       </div>
     );
